efe38512bb7131c9a8240aae15cca8c176af1475
kevin-khoabui <khoabui.kevin@gmail.com>
2026-06-16 10:58:45 +0700

    Update Favicon

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,30 +1,31 @@
 import { ImageResponse } from "next/og";
+
 export const dynamic = "force-static";
 
-// Cấu hình kích thước chuẩn cho Favicon
 export const size = {
   width: 32,
   height: 32,
 };
+
 export const contentType = "image/png";
 
-// Hàm tự động render chữ thành Favicon
 export default function Icon() {
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 18,
-          background: "linear-gradient(to bottom right, #34d399, #059669)", /// Màu xanh Emerald cực hợp với header tối của bạn
+          fontSize: 281,
+          background: "linear-gradient(to bottom right, #34d399, #059669)",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           color: "white",
-          fontWeight: "black",
+          fontWeight: 900,
           fontFamily: "sans-serif",
-          borderRadius: "8px", // Bo tròn góc cho hiện đại giống ứng dụng di động
+          borderRadius: "125px",
+          lineHeight: 1,
         }}
       >
         QA
